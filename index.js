e9b025ec51d5ac9c8398b3680879a6ebc1df4a3a
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2020-11-24 18:21:51 -0500

    cleanup

--- index.js
+++ index.js
@@ -8,46 +8,20 @@ module.exports = class Emojify extends Plugin {
       description: 'Emojify Text',
       usage: '{c} [text to emojify]',
       executor: (args) => ({
-        //var args = "test one two THREE 98778"
-args = args.split("")
-//console.log(args)
-args.forEach(char => {
-    if(char == "1") {
-        output = output + ":one:"
-    }
-    else if(char == "2") {
-        output = output + ":two:"
-    }
-    else if(char == "3") {
-        output = output + ":three:"
-    }
-    else if(char == "4") {
-        output = output + ":four:"
-    }
-    else if(char == "5") {
-        output = output + ":five:"
-    }
-    else if(char == "6") {
-        output = output + ":six:"
-    }
-    else if(char == "7") {
-        output = output + ":seven:"
-    }
-    else if(char == "8") {
-        output = output + ":eight:"
-    }
-    else if(char == "9") {
-        output = output + ":nine:"
-    }
-    else if(char == " ") {
-        output = output + ""
-    }
-    
-    else if (isNaN(parseInt(char))){
-        output= output + ":regional_indicator_" + char.toLowerCase() + ":"
-    }
-})
-//console.log(output)
+        args = args.split("")
+        args.forEach(char => {
+            if(char == "1") {output = output + ":one:"} //will eventually updated switch case
+                else if(char == "2") {output = output + ":two:"}
+                else if(char == "3") {output = output + ":three:"}
+                else if(char == "4") {output = output + ":four:"}
+                else if(char == "5") {output = output + ":five:"}
+                else if(char == "6") {output = output + ":six:"}
+                else if(char == "7") {output = output + ":seven:"}
+                else if(char == "8") {output = output + ":eight:"}
+                else if(char == "9") {output = output + ":nine:"}
+                else if(char == " ") {output = output + " "}
+                else if (isNaN(parseInt(char))){output= output + ":regional_indicator_" + char.toLowerCase() + ":"}
+            })
         send: true,
         result: output
       })
