6a1d354acab26430bebd694f8113c62bf49fcdb6
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2020-11-24 22:09:46 -0500

    still cant spell

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = class Emojify extends Plugin {
               case "9": output += ":nine:";   break
               case " ": output += " ";        break
               case "!": output += ":grey_exclamation:"; break
-              case "?": output += ":grey_question:"; blank
+              case "?": output += ":grey_question:"; break
               default: output += `:regional_indicator_${char.toLowerCase()}:`
             }
             });
