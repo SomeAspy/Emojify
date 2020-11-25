c472a3eb4a2d65c740e5d182a6c1b9de2000a61e
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2020-11-25 13:32:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Plugin } = require('powercord/entities');
-let output = ""
+let out = ""
 module.exports = class Emojify extends Plugin {
   startPlugin () {
     powercord.api.commands.registerCommand({
@@ -7,47 +7,47 @@ module.exports = class Emojify extends Plugin {
       description: 'Emojify Text',
       usage: '{c} [text to emojify]',
       executor: (args) => {
-        output = ""
+        out = ""
         args = args.join(" ").split("")
         args.forEach(char => {
             switch(char){
-              case "0": output += ":zero:";   break
-              case "1": output += ":one:";    break
-              case "2": output += ":two:";    break
-              case "3": output += ":three:";  break
-              case "4": output += ":four:";   break
-              case "5": output += ":five:";   break
-              case "6": output += ":six:";    break
-              case "7": output += ":seven:";  break
-              case "8": output += ":eight:";  break
-              case "9": output += ":nine:";   break
-              case " ": output += " ";        break
-              case "#": output += ":hash:";   break
-              case "|": output += ":clock6:"; break
-              case "'": case "`": output += ":clock12:"; break
-              case ",": output += ":clock630:"; break
-              case "~": output += ":wavy_dash"; break
-              case "-": output += ":no_entry:"; break
-              case "/": output += ":probing_cane:"; break
-              case "(": output += ":waning_crescent_moon:"; break
-              case ")": output += ":waxing_crescent_moon:"; break
-              case "$": output += ":heavy_dollar_sign:"; break
-              case "+": output += ":heavy_plus_sign:"; break
-              case "^": output += ":arrow_up_small:"; break
-              case "<": output += ":arrow_backward:"; break
-              case ">": output += ":arrow_forward:"; break
-              case "*": output += ":asterisk:"; break
-              case "!": output += ":grey_exclamation:"; break
-              case "?": output += ":grey_question:"; break
-              default: char.match(/[a-z]|[A-Z]/) ? output += `:regional_indicator_${char.toLowerCase()}:` : console.log("invalid character!")
+              case "0": out += ":zero:";   break
+              case "1": out += ":one:";    break
+              case "2": out += ":two:";    break
+              case "3": out += ":three:";  break
+              case "4": out += ":four:";   break
+              case "5": out += ":five:";   break
+              case "6": out += ":six:";    break
+              case "7": out += ":seven:";  break
+              case "8": out += ":eight:";  break
+              case "9": out += ":nine:";   break
+              case " ": out += " ";        break
+              case "#": out += ":hash:";   break
+              case "|": out += ":clock6:"; break
+              case "'": case "`": out += ":clock12:"; break
+              case ",": out += ":clock630:"; break
+              case "~": out += ":wavy_dash"; break
+              case "-": out += ":no_entry:"; break
+              case "/": out += ":probing_cane:"; break
+              case "(": out += ":waning_crescent_moon:"; break
+              case ")": out += ":waxing_crescent_moon:"; break
+              case "$": out += ":heavy_dollar_sign:"; break
+              case "+": out += ":heavy_plus_sign:"; break
+              case "^": out += ":arrow_up_small:"; break
+              case "<": out += ":arrow_backward:"; break
+              case ">": out += ":arrow_forward:"; break
+              case "*": out += ":asterisk:"; break
+              case "!": out += ":grey_exclamation:"; break
+              case "?": out += ":grey_question:"; break
+              default: char.match(/[a-z]|[A-Z]/) ? out += `:regional_indicator_${char.toLowerCase()}:` : console.log("invalid character!")
             }
-            });
+          });
       return{
         send: true,
-        result: output
+        result: out
       }
-      }
-    })
+    }
+  })
   }
   pluginWillUnload () {
     powercord.api.commands.unregisterCommand('emojify');
